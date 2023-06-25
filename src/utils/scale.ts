65052872b5a36e5041329615ca6eee311e80ec65
ibrahim taofeek opeyemi <taofeekibrahimope@gmail.com>
2023-06-25 12:23:06 +0100

    setting up components

--- src/utils/scale.ts
+++ src/utils/scale.ts
@@ -7,24 +7,19 @@ import {
 /*=============================================
 =            Dimensions            =
 =============================================*/
-
 export const windowHeight = Dimensions.get('window').height;
 export const windowWidth = Dimensions.get('window').width;
-
 /*=====  End of Dimensions  ======*/
 
 /*=============================================
 =            Dimensions            =
 =============================================*/
-
 const {width, height} = Dimensions.get('window');
-
 /*=====  End of Dimensions  ======*/
 
 /*=============================================
 =            Horizontal - Vertical & Moderate Scaling            =
 =============================================*/
-
 const guidelineBaseWidth = 375;
 const guidelineBaseHeight = 812;
 
@@ -34,13 +29,11 @@ const moderateScale = (size: number, factor = 0.5) =>
   size + (horizontalScale(size) - size) * factor;
 
 export {horizontalScale, verticalScale, moderateScale};
-
 /*=====  End of Horizontal - Vertical & Moderate Scaling  ======*/
 
 /*=============================================
 =   Width - Height Screen Responsiveness       =
 =============================================*/
-
 const customWidth = 375;
 const customHeight = 812;
 
@@ -52,5 +45,4 @@ export const hp = (value: number) => {
   const dimension = (value / customHeight) * 100;
   return hdp(`${dimension}%`);
 };
-
 /*=====  End of Width - Height Screen Responsiveness  ======*/
